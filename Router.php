8b189fff401875637971efd84210aab9aa82d85b
NathanPakenham1 <nathan@nathanpakenham.com>
2025-04-26 15:43:11 +0100

    initial

---
+++ Router.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\core;
+
+use app\core\exception\notFoundException;
+
+class Router
+{
+
+    public Request $request;
+    public Response $response;
+    protected array $routes = [];
+
+    public function __construct(Request $request, Response $response)
+    {
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function post($path, $callback)
+    {
+        $this->routes['post'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPath();
+        $method = $this->request->method();
+        $callback = $this->routes[$method][$path] ?? false;
+        if ($callback === false) {
+            throw new notFoundException();
+        }
+        if (is_string($callback)) {
+            return Application::$app->view->renderView($callback);
+        }
+        if (is_array($callback)) {
+            $controller = new $callback[0]();
+            $controller->action = $callback[1];
+            Application::$app->controller = $controller;
+            $middlewares = $controller->getMiddlewares();
+            foreach ($middlewares as $middleware) {
+                $middleware->execute();
+            }
+            // Replace the class name with the instance for call_user_func
+            $callback[0] = $controller;
+        }
+
+        return call_user_func($callback, $this->request, $this->response);
+    }
+}
